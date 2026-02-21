c141168e564f5967e9359c9503c1d691b3ad283a
bob <bob>
2026-02-21 11:42:27 +0100

    Finita la finestra popup come messagebox

---
+++ Classes/View/FinestraPopUP.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InTempo.Classes.View
+{
+    /// <summary>
+    /// Logica di interazione per FinestraPopUP.xaml
+    /// </summary>
+    public partial class FinestraPopUP : Window
+    {
+        public string Titolo { get; set; }
+        public string Testo { get; set; }
+
+        public bool Ritorno { get; set; } = true;
+
+
+
+
+
+        public FinestraPopUP(string TitoloPassato, string TestoPassato, int Bottoni)
+        {
+            InitializeComponent();
+
+            switch (Bottoni)
+            {
+                case 0:
+                    BtnPredefinito1.Visibility = Visibility.Collapsed;
+                    BtnPredefinito2.Visibility = Visibility.Collapsed;
+
+                    break;
+                case 1:
+                    BtnPredefinito2.Visibility = Visibility.Visible;
+                    BtnPredefinito1.Visibility = Visibility.Collapsed;
+
+                    break;
+                case 2:
+                    BtnPredefinito1.Visibility = Visibility.Visible;
+                    BtnPredefinito2.Visibility = Visibility.Visible;
+                    break;
+            }
+            Titolo = TitoloPassato;
+            Testo = TestoPassato;
+
+        }
+
+        public FinestraPopUP(string TitoloPassato, string TestoPassato, string TestoBtn1, string TestoBtn2)
+        {
+            InitializeComponent();
+            Titolo = TitoloPassato;
+            Testo = TestoPassato;
+            BtnPredefinito1.Content = TestoBtn1;
+            BtnPredefinito2.Content = TestoBtn2;
+        }
+
+        private void BtnPredefinito2_Click(object sender, RoutedEventArgs e)
+        {
+            Ritorno = true;
+        }
+
+        private void BtnPredefinito1_Click(object sender, RoutedEventArgs e)
+        {
+            Ritorno = false;
+        }
+    }
+}
